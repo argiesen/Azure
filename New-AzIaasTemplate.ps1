8b6a4f607acd97157a3ad4d6e451e53813ad97a4
Andy Giesen <argiesen@gmail.com>
2020-07-06 14:46:46 -0700

    Update New-AzIaasTemplate.ps1

--- New-AzIaasTemplate.ps1
+++ New-AzIaasTemplate.ps1
@@ -6,8 +6,16 @@ $VnetAsgSubnet = "10.254.0.16/28"
 $VnetServer01Subnet = "10.254.10.0/24"
 $VnetDmz01Subnet = "10.254.20.0/24"
 
+#Create network RG
 New-AzResourceGroup -Name rg-net01-prd-$AzRegion -Location $AzRegion
 
+#Create VPN gateway subnet
+$vpnSubnet = New-AzVirtualNetworkSubnetConfig -Name snet-gateway01-prd-$AzRegion -AddressPrefix $VnetGatewaySubnet
+
+#Create ASG subnet
+$asgSubnet = New-AzVirtualNetworkSubnetConfig -Name snet-asg01-prd-$AzRegion -AddressPrefix $VnetAsgSubnet
+
+#Create server subnet and NSG
 $serverRule1 = New-AzNetworkSecurityRuleConfig -Name rdp-rule -Description "Allow RDP from internal" `
 	-Direction Inbound `
 	-Priority 100 `
@@ -29,6 +37,9 @@ $serverRule2 = New-AzNetworkSecurityRuleConfig -Name ssh-rule -Description "Allo
 $nsgServer = New-AzNetworkSecurityGroup -Name nsg-server01-prd-$AzRegion -Location $AzRegion -ResourceGroupName rg-net01-prd-$AzRegion -SecurityRules $serverRule1,$serverRule2
 $snetServerId = (Get-AzNetworkSecurityGroup -Name nsg-server01-prd-$AzRegion -ResourceGroupName rg-net01-prd-eastus2).Id
 
+$srvSubnet = New-AzVirtualNetworkSubnetConfig -Name snet-server01-prd-$AzRegion -AddressPrefix $VnetServer01Subnet -NetworkSecurityGroupId $snetServerId
+
+#Create DMZ subnet and NSG
 $dmzRule1 = New-AzNetworkSecurityRuleConfig -Name rdp-rule -Description "Allow RDP from internal" `
 	-Direction Inbound `
 	-Priority 100 `
@@ -59,17 +70,16 @@ $dmzRule3 = New-AzNetworkSecurityRuleConfig -Name web-rule -Description "Allow H
 New-AzNetworkSecurityGroup -Name nsg-dmz01-prd-$AzRegion -Location $AzRegion -ResourceGroupName rg-net01-prd-$AzRegion -SecurityRules $dmzRule1,$dmzRule2,$dmzRule3
 $snetDmzId = (Get-AzNetworkSecurityGroup -Name nsg-dmz01-prd-eastus2 -ResourceGroupName rg-net01-prd-eastus2).Id
 
-$vpnSubnet = New-AzVirtualNetworkSubnetConfig -Name snet-gateway01-prd-$AzRegion -AddressPrefix $VnetGatewaySubnet
-$asgSubnet = New-AzVirtualNetworkSubnetConfig -Name snet-asg01-prd-$AzRegion -AddressPrefix $VnetAsgSubnet
-$srvSubnet = New-AzVirtualNetworkSubnetConfig -Name snet-server01-prd-$AzRegion -AddressPrefix $VnetServer01Subnet -NetworkSecurityGroupId $snetServerId
 $dmzSubnet = New-AzVirtualNetworkSubnetConfig -Name snet-dmz01-prd-$AzRegion -AddressPrefix $VnetDmz01Subnet -NetworkSecurityGroupId $snetDmzId
+
+#Create virtual network with subnets
 New-AzVirtualNetwork -Name vnet-prd-$AzRegion -Location $AzRegion -ResourceGroupName rg-net01-prd-$AzRegion -AddressPrefix $VnetAddressSpace -Subnet $vpnSubnet,$asgSubnet,$srvSubnet,$dmzSubnet
 
-
+#Create storage RG
 New-AzResourceGroup -Name rg-storage01-prd-$AzRegion -Location $AzRegion
 #diag storage account
 #key vault
 
-
+#Create VM RG
 New-AzResourceGroup -Name rg-vm01-prd-$AzRegion -Location $AzRegion
 
